711d42a4ae96162557c90ba26da586c88f5d763f
fahad alajmi <fahadalajmi126@gmail.com>
2022-01-11 00:54:14 +0300

    final draft

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const emptyArrays =[
+let emptyArrays =[
 
 ];
 let hobbies = [
@@ -11,11 +11,14 @@ let hobbies = [
 ];
 let numbers = [7,17,77,26,23];
 console.log(numbers[1]);
-console.log(hobbies[5]);
+console.log(hobbies[hobbies.length-1]);
 console.log(hobbies.length);
 hobbies.pop();
 hobbies.pop();
-console.log(hobbies);
+console.log(hobbies[0]);
+console.log(hobbies[1]);
+console.log(hobbies[2]);
+console.log(hobbies[3]);
 console.log(hobbies.length);
 hobbies =[];
 console.log(hobbies.length);
