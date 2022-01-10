12bf94b0115334666e399aeab3e590b5f0e5c6db
fahad alajmi <fahadalajmi126@gmail.com>
2022-01-10 19:43:13 +0300

    2nd draft

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ let hobbies = [
 "tennis"
 ];
 let numbers = [7,17,77,26,23];
-console.log(numbers.slice(1,2));
-console.log(hobbies.slice(5));
+console.log(numbers[1]);
+console.log(hobbies[5]);
 console.log(hobbies.length);
-hobbies.splice(4,5);
+hobbies.pop();
+hobbies.pop();
 console.log(hobbies);
 console.log(hobbies.length);
 hobbies =[];
