f5a7b143d70db7b87a83214b15644c9bc26380fd
fahad alajmi <fahadalajmi126@gmail.com>
2022-01-10 18:56:01 +0300

    first draft

---
+++ index.js
@@ -0,0 +1,20 @@
+const emptyArrays =[
+
+];
+let hobbies = [
+"reading",
+"working out",
+"boxing",
+"running",
+"vollyball",
+"tennis"
+];
+let numbers = [7,17,77,26,23];
+console.log(numbers.slice(1,2));
+console.log(hobbies.slice(5));
+console.log(hobbies.length);
+hobbies.splice(4,5);
+console.log(hobbies);
+console.log(hobbies.length);
+hobbies =[];
+console.log(hobbies.length);
